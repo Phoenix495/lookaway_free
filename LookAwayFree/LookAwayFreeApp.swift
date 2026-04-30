c64978aecd3efc104063940b61b2968b9fa4b8c6
Vitalii Serheiev <vitalii.serheiev@skylum.com>
2026-04-30 12:07:59 +0200

    Initial Commit

---
+++ LookAwayFree/LookAwayFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookAwayFreeApp.swift
+//  LookAwayFree
+//
+//  Created by Vitalii Serheiev on 30.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct LookAwayFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LookAwayFreeTests/LookAwayFreeTests.swift
@@ -0,0 +1,19 @@
+//
+//  LookAwayFreeTests.swift
+//  LookAwayFreeTests
+//
+//  Created by Vitalii Serheiev on 30.04.2026.
+//
+
+import Testing
+@testable import LookAwayFree
+
+struct LookAwayFreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
